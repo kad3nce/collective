871177a18af9a67ce494e34f7b7b49c5441a6a33
Jed Hurt <jedhurt@cableone.net>
2008-04-01 16:48:46 -0600

    Words

--- db/seed_dev.rb
+++ db/seed_dev.rb
@@ -3,7 +3,7 @@ Merb.logger.info('Seeding development database')
 Page.create! \
   :name => 'Isolated controller/view specifications', 
   :content => <<-CONTENT
-You have multiple options for accomplishing right now, as it's not officially supported:
+You a few hackish options for accomplishing this right now, as it's not yet officially supported:
 
 * Use "Tim Connor's Hack":http://www.timocracy.com/articles/2007/12/17/isolated-controller-and-view-testing-in-merb
 * Roll your own helper that short-circuits the render method of the controller mixin
