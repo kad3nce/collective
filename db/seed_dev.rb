d7ac5e06f800866cf52083ad5c2bb5bee6f0843a
Jed Hurt <jedhurt@cableone.net>
2008-04-02 02:50:44 -0600

    Update seed data to reflect current schema

--- db/seed_dev.rb
+++ db/seed_dev.rb
@@ -1,9 +1,11 @@
 Merb.logger.info('Seeding development database')
 
-Page.create! \
+speccing_page = Page.create! \
   :name => 'Isolated controller/view specifications', 
-  :content => <<-CONTENT
-You a few hackish options for accomplishing this right now, as it's not yet officially supported:
+  :content => 'Coming soon...'
+
+speccing_page.update_attributes :content => <<-CONTENT
+You have a few options for accomplishing this right now, as it's not quite yet officially supported:
 
 * Use "Tim Connor's Hack":http://www.timocracy.com/articles/2007/12/17/isolated-controller-and-view-testing-in-merb
 * Roll your own helper that short-circuits the render method of the controller mixin
