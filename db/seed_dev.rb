ef7f1022f670f09a3d80269f87995fba3e588e32
Jed Hurt <jedhurt@cableone.net>
2008-04-04 17:50:47 -0600

    Add some more seed data

--- db/seed_dev.rb
+++ db/seed_dev.rb
@@ -10,3 +10,16 @@ You have a few options for accomplishing this right now, as it's not quite yet o
 * Use "Tim Connor's Hack":http://www.timocracy.com/articles/2007/12/17/isolated-controller-and-view-testing-in-merb
 * Roll your own helper that short-circuits the render method of the controller mixin
 CONTENT
+
+speccing_page.update_attributes :content => <<-CONTENT
+For isolated controller specs:
+
+* create an instance of your controller
+* stub its render method
+* Stub the MyController#new class method to always return your stubbed controller
+<pre>
+  my_stubbed_controller = MyController.new
+  my_stubbed_controller.stub!(:render).and_return 'nothing special'
+  MyController.stub!(:new).and_return my_stubbed_controller
+</pre>
+CONTENT
