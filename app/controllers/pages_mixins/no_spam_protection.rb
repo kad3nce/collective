af2012e9251e0026af46649d9bf556ff5ee26a40
Jed Hurt <jedhurt@cableone.net>
2008-04-06 15:45:51 -0600

    Forego the use of action-args until we find a solution for using it with mixed-in actions

--- app/controllers/pages_mixins/no_spam_protection.rb
+++ app/controllers/pages_mixins/no_spam_protection.rb
@@ -1,10 +1,13 @@
+# =========================================================================
+# = TODO: Figure out a way to get action-args to work on mixed in actions =
+# =========================================================================
 module NoSpamProtection
   def self.included(base)
     base.show_action(:create, :update)
   end
   
-  def create(page)
-    @page = Page.new page
+  def create
+    @page = Page.new params[:page]
     if @page.save
       redirect url(:page, @page)
     else
@@ -12,9 +15,9 @@ module NoSpamProtection
     end
   end
 
-  def update(id, page)
-    @page = Page.first(:slug => id) || raise(NotFound)
-    if @page.update_attributes(:content => page[:content])
+  def update
+    @page = Page.first(:slug => params[:id]) || raise(NotFound)
+    if @page.update_attributes(:content => params[:page][:content])
       redirect url(:page, @page)
     else
       raise BadRequest
