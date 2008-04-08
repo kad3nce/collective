57d9c6cbcfef2d58743e226af8d107098d4883ec
James Herdman <james.herdman@gmail.com>
2008-04-08 12:52:54 -0400

    Specs for /index on Edits

--- app/helpers/edits_helper.rb
+++ app/helpers/edits_helper.rb
@@ -1,21 +1,31 @@
 module Merb
-    module EditsHelper
-      def spam_or_ham(version)
-        version.spam ? 'spam' : 'ham'
-      end
-      
-      def link_to_page_or_nothing(version)
-        if(page = version.page)
-          link_to page.name, url(:page, page)
-        else
-          "New Page: #{version.content.split(':').last}"
-        end
-      end
-      
-      def mark_as_ham_or_spam(version)
-        open_tag('button', :type => 'submit') +
-          (version.spam ? 'This Is Ham' : 'This Is Spam') + 
-          "</button>"
+  module EditsHelper
+    def spam_or_ham(version)
+      version.spam ? 'spam' : 'ham'
+    end
+    
+    #--
+    # FIXME This method explodes if Version is nil, or Version does not have 
+    # any contents
+    def link_to_page_or_nothing(version)
+      if(page = version.page)
+        link_to page.name, url(:page, page)
+      else
+        "New Page: #{version.content.split(':').last}"
       end
     end
+    
+    def mark_as_ham_or_spam(version)
+      [
+        open_tag('button', :type => 'submit'), 
+        spam_or_ham_label(version), 
+        "</button>"
+      ].join("")
+    end
+    
+  private
+    def spam_or_ham_label(version)
+      version.spam ? 'This Is Ham' : 'This Is Spam'
+    end
+  end
 end
