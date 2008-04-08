b18a9d1799218614e00cf060dabf31f225c96665
Jed Hurt <jedhurt@cableone.net>
2008-04-07 18:17:07 -0600

    Added helpers to highlight navigation links for current page and to change the page title

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -1,5 +1,20 @@
 module Merb
   module GlobalHelpers
-    # helpers defined here available to all views.  
+    def highlighted_link_to(title, url)
+      opts = {}
+      opts.merge!(:id => 'active') if @page_title && @page_title == title
+      link_to(title, url, opts)
+    end
+    
+    def title(options)
+      if(options.is_a?(Hash))
+        merged_title = @site_title = options[:site]
+        (merged_title << " | #{@page_title}") if @page_title
+        return merged_title
+      else
+        @page_title = options
+        return @page_title
+      end
+    end
   end
 end
