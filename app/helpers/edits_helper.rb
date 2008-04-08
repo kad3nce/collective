acac765d9a4b100c249a4d15fc38ee7c0028d267
Jed Hurt <jedhurt@cableone.net>
2008-04-08 04:01:03 -0600

    Added link_to_page_or_nothing method to edits helper. This is need in case a Version doesn't belong to a page (someone tried to create a new page that was spam)

--- app/helpers/edits_helper.rb
+++ app/helpers/edits_helper.rb
@@ -4,6 +4,14 @@ module Merb
         version.spam ? 'spam' : 'ham'
       end
       
+      def link_to_page_or_nothing(version)
+        if(page = version.page)
+          link_to page.name, url(:page, page)
+        else
+          "New Page: #{version.content.split(':').last}"
+        end
+      end
+      
       def mark_as_ham_or_spam(version)
         open_tag('button', :type => 'submit') +
           (version.spam ? 'This Is Ham' : 'This Is Spam') + 
