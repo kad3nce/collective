3797562d907461d16450ac27a4dbef1d451d4caf
Jed Hurt <jedhurt@cableone.net>
2008-04-08 02:13:01 -0600

    Created user interface to view recent edits and mark them as ham or spam (in case of false negatives/positives)

--- app/controllers/edits.rb
+++ app/controllers/edits.rb
@@ -1,19 +1,23 @@
 class Edits < Application
   def index
-    @edits = Version.all
+    @edits = Version.all(:moderated => false, :limit => 100, :order => 'created_at DESC')
     display @edits
   end
 
   def update
     @edit = Version.first(params[:id])
-    raise NotFound unless @spam
+    raise NotFound unless @edit
     if @edit.update_attributes(:moderated => true)
       if(@edit.spam)
         # send @edit.signature to Defensio with #report-false-positives
       else
         # send @edit.signature to Defensio with #report-false-negatives
       end
-      redirect url(:edits)
+      if request.xhr?
+        render :nothing => 200
+      else
+        redirect url(:edits)
+      end
     else
       raise BadRequest
     end
--- app/helpers/edits_helper.rb
+++ app/helpers/edits_helper.rb
@@ -1,7 +1,13 @@
 module Merb
-    module SpamsHelper
+    module EditsHelper
       def spam_or_ham(version)
         version.spam ? 'spam' : 'ham'
+      end
+      
+      def mark_as_ham_or_spam(version)
+        open_tag('button', :type => 'submit') +
+          (version.spam ? 'Mark as Ham' : 'Mark as Spam') + 
+          "</button>"
       end
     end
 end
