8802fa1bae8e655793034dea3c09f070652f8473
James Herdman <james.herdman@gmail.com>
2008-04-08 13:13:47 -0400

    Version has been spec'd out a little cleaner and better, and added a method to find the most recent records.

--- spec/models/version_spec.rb
+++ spec/models/version_spec.rb
@@ -1,32 +1,75 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe Version do
-  before(:each) do
-    @page = stub_everything('page')
-  end
   
-  it 'should have a content field' do
-    Version.new(:content => 'How to make Merb cook breakfast...').content.should == 'How to make Merb cook breakfast...'
-  end
-  
-  it 'should have a content_html field' do
-    Version.new(:content_html => '<p>How to make Merb cook breakfast...</p>').content_html.should == '<p>How to make Merb cook breakfast...</p>'
+  describe ".new" do
+    attr_accessor :page
+
+    before(:each) do
+      self.page = stub_everything('page')
+    end
+
+    after(:each) do
+      self.page = nil
+    end
+    
+    it 'should have a content field' do
+      Version.new(:content => 'How to make Merb cook breakfast...').content.should == 'How to make Merb cook breakfast...'
+    end
+
+    it 'should have a content_html field' do
+      Version.new(:content_html => '<p>How to make Merb cook breakfast...</p>').content_html.should == '<p>How to make Merb cook breakfast...</p>'
+    end
+    
+    it 'should have a created_at field' do
+      creation_time = Time.now
+      Version.new(:created_at => creation_time).created_at.should == creation_time
+    end
+
+    it 'should have a number field' do
+      Version.new(:number => 10).number.should == 10
+    end
+
+    it 'should belong to a page' do
+      Version.new(:page => page).page.should == page
+    end
   end
 
-  it 'should have a created_at field' do
-    creation_time = Time::now
-    Version.new(:created_at => creation_time).created_at.should == creation_time
+  describe "#populate_html_content" do
+    
+    attr_accessor :version
+    
+    before(:each) do
+      Version.publicize_methods do
+        self.version = Version.new(:content => "How to make Merb cook breakfast")
+        version.populate_content_html
+      end
+    end
+    
+    after(:each) do
+      self.version = nil
+    end
+    
+    it 'should render content to HTML' do
+      version.content_html.should match(/^<p>How to make/)
+    end
   end
 
-  it 'should have a number field' do
-    Version.new(:number => 10).number.should == 10
+  describe ".most_recent_unmoderated" do
+    attr_accessor :versions
+    
+    before(:each) do
+      self.versions = []
+    end
+    
+    after(:each) do
+      self.versions = nil
+    end
+    
+    it "should find the most recent Versions" do
+      Version.should_receive(:all).and_return(versions)
+      Version.most_recent_unmoderated.should == versions
+    end
   end
 
-  it 'should belong to a page' do
-    Version.new(:page => @page).page.should == @page
-  end
-  
-  it 'should populate content_html before saving by converting its content attribute to html' do
-    Version.create!(:content => 'How to make Merb cook breakfast...').content_html.should == '<p>How to make Merb cook breakfast&#8230;</p>'
-  end
 end
