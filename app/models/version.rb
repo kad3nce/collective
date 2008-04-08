6df2f903e8290dc08dc0f92d41c207eb06ac9ce5
Jed Hurt <jedhurt@cableone.net>
2008-04-07 18:15:56 -0600

    Tightening up whitespace

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,24 +1,17 @@
 class Version < DataMapper::Base
-  
-  ## Properties
-  
   property :content,      :text
   property :content_html, :text
   property :created_at,   :datetime
   property :number,       :integer
-  
-  ## Associations
+  property :spam,         :boolean, :default => false
+  property :spaminess,    :float, :default => 0
   
   belongs_to :page
-  
-  ## Call-backs
   
   before_save :populate_content_html
   
 private
-
   def populate_content_html
     self.content_html = RedCloth.new(content).to_html
   end
-
 end
