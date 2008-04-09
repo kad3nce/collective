09ba7ecb0ca07c18cb4d2cd30c4730867ae22c45
James Herdman <james.herdman@gmail.com>
2008-04-09 17:06:35 -0400

    Moved model specific logic into the model. Fat models, skinny controllers.

--- app/models/version.rb
+++ app/models/version.rb
@@ -12,6 +12,8 @@ class Version < DataMapper::Base
   
   before_save :populate_content_html
   
+  after_save  :update_as_spam_or_ham
+
   def self.most_recent_unmoderated(max=100)
     all(:moderated => false, :limit => max, :order => 'created_at DESC')
   end
@@ -19,5 +21,17 @@ class Version < DataMapper::Base
 private
   def populate_content_html
     self.content_html = RedCloth.new(content).to_html
+  end
+  
+  def update_as_spam_or_ham
+    # FIXME Refactoring opportunity:
+    # This sort of logic should be shoved inside of Viking. If models that are 
+    # protected using Viking have a certain assumed API, we can keep any sort 
+    # of logic like the below where it belongs: in Viking.
+    if spam?
+      DEFENSIO_GATEWAY.mark_as_ham(:signatures => signature)
+    else
+      DEFENSIO_GATEWAY.mark_as_spam(:signatures => signature)
+    end
   end
 end
