946750abfe5077c91ed79021f7abea2ddf08e2b3
James Herdman <james.herdman@gmail.com>
2008-04-12 12:54:37 -0400

    Most of the 'show' method is specd. There's a pretty lousy API choice in this method that I'm going to factor out and into the Page model before continuing further.

--- spec/controllers/pages_spec.rb
+++ spec/controllers/pages_spec.rb
@@ -7,6 +7,8 @@ describe Pages do
   before(:each) do
     self.page = mock_model(Page)
     self.pages = [page]
+    
+    Page.stub!(:by_slug).and_return(page)
   end
   
   after(:each) do
@@ -37,6 +39,38 @@ describe Pages do
     it "should display all pages" do
       dispatch_to(Pages, :index) do |controller|
         controller.should_receive(:display).with(pages)
+      end
+    end
+  end
+
+  describe "requesting /pages/1 with GET" do
+    def do_get
+      dispatch_to(Pages, :show, :id => "1") do |controller|
+        controller.stub!(:display)
+      end
+    end
+    
+    it "should be successful" do
+      do_get.should be_successful
+    end
+    
+    it "should load the requested Page by the specified slug" do
+      Page.should_receive(:by_slug).and_return(page)
+      do_get.assigns(:page).should == page
+    end
+    
+    it "should raise NotFound if a record cannot be found with the specified slug" do
+      Page.should_receive(:by_slug).and_return(nil)
+      lambda { do_get }.should raise_error(Merb::ControllerExceptions::NotFound)
+    end
+    
+    it "should load the latest Version for this Page by default" 
+    
+    it "should be able to load the requested Version for this Page" 
+    
+    it "should display the Page record" do
+      dispatch_to(Pages, :show, :id => "1") do |controller|
+        controller.should_receive(:display).with(page)
       end
     end
   end
