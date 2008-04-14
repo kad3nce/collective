135993cd3a8ab4e400c13bf411497c02c1bfe4f5
James Herdman <james.herdman@gmail.com>
2008-04-14 00:20:09 -0400

    Stop using old method of marking spam

--- app/models/version.rb
+++ app/models/version.rb
@@ -11,13 +11,11 @@ class Version < DataMapper::Base
   belongs_to :page
   
   before_save :populate_content_html
-  
-  after_save  :update_as_spam_or_ham
 
   def spam_or_ham
     spam? ? "spam" : "ham"
   end
-
+  
   def self.most_recent_unmoderated(max=100)
     all(:moderated => false, :limit => max, :order => 'created_at DESC')
   end
@@ -29,17 +27,5 @@ class Version < DataMapper::Base
 private
   def populate_content_html
     self.content_html = RedCloth.new(content).to_html
-  end
-  
-  def update_as_spam_or_ham
-    # FIXME Refactoring opportunity:
-    # This sort of logic should be shoved inside of Viking. If models that are 
-    # protected using Viking have a certain assumed API, we can keep any sort 
-    # of logic like the below where it belongs: in Viking.
-    if spam?
-      DEFENSIO_GATEWAY.mark_as_ham(:signatures => signature)
-    else
-      DEFENSIO_GATEWAY.mark_as_spam(:signatures => signature)
-    end
   end
 end
