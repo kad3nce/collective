ff867108ee23d7c029f7106944189083159f5175
James Herdman <james.herdman@gmail.com>
2008-04-14 02:24:50 -0400

    This should happen before the app loads

--- config/initializers/initialize_viking.rb
+++ config/initializers/initialize_viking.rb
@@ -3,7 +3,9 @@ unless Object.const_defined?("Viking")
   require Merb.root / 'lib' / 'viking' / 'viking'
 end
 
-Merb::BootLoader.after_app_loads do
+# This needs to load before the app so Pages can load the appropriate module 
+# for it's +create+ and +update+ methods
+Merb::BootLoader.before_app_loads do
   
   config_path = Merb.root / 'config' / 'spam_protection.yml'
   
