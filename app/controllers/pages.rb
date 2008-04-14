8cd11963e4eefa899e8a46e6ab0435bc8b64302e
James Herdman <james.herdman@gmail.com>
2008-04-14 02:48:34 -0400

    Improve docs

--- app/controllers/pages.rb
+++ app/controllers/pages.rb
@@ -1,21 +1,29 @@
 class Pages < Application
   include(Viking.enabled? ? SpamProtection : NoSpamProtection)
   
+  # Accessed by: GET /pages
   def index
     @pages = Page.all
     display @pages
   end
 
+  # Accessed by: 
+  # - GET /pages/1
+  # - GET /pages/1?version=3
   def show(id, version = :latest)
     @page = Page.by_slug_and_select_version!(id, version) || raise(NotFound)
     display @page
   end
 
+  # Accessed by: GET /pages/new
   def new
     @page = Page.new(:name => 'New Page')
     render
   end
 
+  # Accessed by: 
+  # - GET /pages/1/edit
+  # - GET /pages/1/edit?version=3
   def edit(id, version = :latest)
     @page = Page.by_slug_and_select_version!(id, version) || raise(NotFound)
     render
