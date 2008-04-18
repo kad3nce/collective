55d2831338940b3fe65a685ca35dfbd8ffce2e2a
James Herdman <james.herdman@gmail.com>
2008-04-18 16:44:03 -0400

    Rudimentary Ruby support for Chili

---
+++ public/javascripts/chili/recipes/ruby.js
@@ -0,0 +1,18 @@
+/*
+===============================================================================
+Ruby syntax highlighting support for Chili. This is based on the efforts of 
+Dan Webb and Shelly Fisher in the CodeHighlighter JS library. Please see 
+http://svn.danwebb.net/external/CodeHighlighter/ for more details.
+-------------------------------------------------------------------------------
+LICENSE: http://www.opensource.org/licenses/mit-license.php
+WEBSITE: http://noteslog.com/chili/
+===============================================================================
+*/
+{
+  steps: {
+    com     : { exp : /#[^\n]+/ }, 
+    string  : { exp : /'[^']*'|"[^"]*"/ }, 
+    keyword : { exp : /\b(do|end|self|class|def|if|module|yield|then|else|for|until|unless|while|elsif|case|when|break|retry|redo|rescue|require|raise)\b/ }, 
+    symbol  : { exp : /([^:])(:[A-Za-z0-9_!?]+)/ }
+  }
+}
