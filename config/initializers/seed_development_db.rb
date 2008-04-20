ed644181bc8996e6406165558e52dc3cc46f8f17
Jed Hurt <jedhurt@cableone.net>
2008-04-20 15:10:26 -0600

    Ensure that we don't try to seed the database before it exists.

--- config/initializers/seed_development_db.rb
+++ config/initializers/seed_development_db.rb
@@ -1,7 +1,9 @@
 Merb::BootLoader.after_app_loads do
   if Merb.environment == 'development'
-    # Merb.logger.info('Auto migrating development database')
-    # DataMapper::Persistence.auto_migrate! 
-    require Merb.root / 'db' / 'seed_dev' unless Page.count > 0
+    begin
+      require Merb.root / 'db' / 'seed_dev' unless Page.count > 0
+    rescue DataObject::QueryError
+      # Database not set up yet; don't try to seed it.
+    end
   end
 end
